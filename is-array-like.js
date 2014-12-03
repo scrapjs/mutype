90248f00b0353d296266434e5bbee0686df1f561
Dmitry <df.creative@gmail.com>
2014-12-04 01:58:40 +0300

    Move out bad requirements

--- is-array-like.js
+++ is-array-like.js
@@ -1,4 +1,8 @@
+var isString = require('./is-string');
+var isArray = require('./is-array');
+var isFn = require('./is-fn');
+
 //FIXME: add tests from http://jsfiddle.net/ku9LS/1/
 module.exports = function (a){
-	return require('./is-array')(a) || (a && !require('./is-string')(a) && !a.nodeType && (typeof window != 'undefined' ? a != window : true) && !require('./is-fn')(a) && typeof a.length === 'number');
+	return isArray(a) || (a && !isString(a) && !a.nodeType && (typeof window != 'undefined' ? a != window : true) && !isFn(a) && typeof a.length === 'number');
 }
--- is-plain.js
+++ is-plain.js
@@ -1,3 +1,7 @@
+var isString = require('./is-string'),
+	isNumber = require('./is-number'),
+	isBool = require('./is-bool');
+
 module.exports = function isPlain(a){
-	return !a || require('./is-string')(a) || require('./is-number')(a) || require('./is-bool')(a);
-}
+	return !a || isString(a) || isNumber(a) || isBool(a);
+};
